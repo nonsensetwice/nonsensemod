7be94002f214464ce5464885fdd708980ca26e7b
nonsense <joshua@nonsensecodes.io>
2023-10-05 13:26:52 -0700

    Initial commit

---
+++ src/app/utils/MongoDBUtils.js
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import Log from './Log.js';
+
+const mongoDBURIPartial = `${process.env.MONGODB_PREFIX}://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}/`;
+const options = {
+    appname: process.env.APP_NAME,
+    serverAPI: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    },
+    writeConcern: {
+        w: 'majority'
+    }
+}
+
+const mongoClient = new MongoClient(mongoDBURIPartial, options);
+
+export default async function connect(database) {
+        try {
+
+            Log.debug(`Connecting to ${database} for the first time!`);
+
+            await mongoClient.connect();
+            
+            await mongoClient.db(database).command({ ping: 1 });
+            Log.debug('Pinged your deployment. You successfully connected to MongoDB!');
+
+        } finally {
+            await mongoClient.close();
+        }
+    };
