92b5a3003b0e2d4d0514fb88256d5583c166ced5
nonsensetwice <joshua@nonsensecodes.io>
2023-10-11 16:47:00 -0700

    added hentai command, nsfw folder. linted code. updated docker/docker compose files.

--- src/app/commands/fun/Ping.js
+++ src/app/commands/fun/Ping.js
@@ -2,28 +2,32 @@ import { isMessageInstance } from '@sapphire/discord.js-utilities';
 import { Command } from '@sapphire/framework';
 
 export default class Ping extends Command {
-    constructor(ctx) {
-        super(ctx, {
-            name: 'ping',
-            description: 'Replies with "Pong"'
-        });
-    }
+	constructor(ctx) {
+		super(ctx, {
+			name: 'ping',
+			description: 'Replies with "Pong"'
+		});
+	}
 
-    registerApplicationCommands(registry) {
-        registry.registerChatInputCommand((builder) =>
-          builder.setName('ping').setDescription('Ping bot to see if it is alive')
-        );
-    }
+	registerApplicationCommands(registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName('ping')
+				.setDescription('Ping bot to see if it is alive'),
+		{
+			idHints: ['1159659016855748689']
+		});
+	}
 
-    async chatInputRun(interaction) {
-        const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
-    
-        if (isMessageInstance(msg)) {
-          const diff = msg.createdTimestamp - interaction.createdTimestamp;
-          const ping = Math.round(this.container.client.ws.ping);
-          return interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
-        }
-    
-        return interaction.editReply('Failed to retrieve ping :(');
-    }
+	async chatInputRun(interaction) {
+		const msg = await interaction.reply({ content: 'Ping?', ephemeral: true, fetchReply: true });
+	
+		if (isMessageInstance(msg)) {
+			const diff = msg.createdTimestamp - interaction.createdTimestamp;
+			const ping = Math.round(this.container.client.ws.ping);
+			return interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
+		}
+	
+		return interaction.editReply('Failed to retrieve ping :(');
+	}
 }
